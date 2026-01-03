610851f85bd739230fd68af1b60a34f25414bd3e
Ann Schulte <aschulte1919@gmail.com>
2026-01-03 17:02:07 -0600

    new version

--- plexus/__init__.py
+++ plexus/__init__.py
@@ -27,5 +27,5 @@ Auto-Detection:
 from plexus.client import Plexus
 from plexus.config import load_config, save_config
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = ["Plexus", "load_config", "save_config"]
