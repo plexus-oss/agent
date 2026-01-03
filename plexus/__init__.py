dd11f533d5b469fea89059a63298622260ef0867
Ann Schulte <aschulte1919@gmail.com>
2026-01-03 14:05:37 -0600

    version

--- plexus/__init__.py
+++ plexus/__init__.py
@@ -11,5 +11,5 @@ Usage:
 from plexus.client import Plexus
 from plexus.config import load_config, save_config
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["Plexus", "load_config", "save_config"]
